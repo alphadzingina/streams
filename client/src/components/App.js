47d800052db4821f67bcd35c23501240401380b0
alphadzingina <alphaadx6@gmail.com>
2020-06-20 19:47:10 +0100

    wire up routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import StreamEdit from './streams/StreamEdit';
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
 
 const App = () => {
-    return(
-        <div>App</div>
+    return (
+        <div>
+            <BrowserRouter>
+                <div>
+                    <Route path="/" exact component={StreamList} />
+                    <Route path="/streams/show" exact component={StreamShow} />
+                    <Route path="/streams/edit" exact component={StreamEdit} />
+                    <Route path="/streams/new" exact component={StreamCreate} />
+                    <Route path="/streams/delete" exact component={StreamDelete} />
+                </div>
+            </BrowserRouter>
+        </div>
     );
 };
 
