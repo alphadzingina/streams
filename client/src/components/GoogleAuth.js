4a6d1a8b4c904f874be10d54089354cd4e8cbd92
alphadzingina <alphaadx6@gmail.com>
2020-06-23 22:27:34 +0100

    wire up google api library

---
+++ client/src/components/GoogleAuth.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+class GoogleAuth extends React.Component {
+    state = { isSignedIn: null };
+
+    componentDidMount() {
+        window.gapi.load('client:auth2', () => {
+            window.gapi.client.init({
+                clientId: '249046963164-uip819c17l50rgo4uev0famrd2j3v4h2.apps.googleusercontent.com',
+                scope: 'email'
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+                this.auth.isSignedIn.listen(this.onAuthChange);
+            });
+        });
+    }
+
+    onAuthChange = () => {
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+    }
+    
+    renderAuthButton() {
+        if (this.state.isSignedIn === null ) {
+            return (
+                <div>I don't know if we are signed in</div>
+            );
+        } 
+        else if ( this.state.isSignedIn === true ) {
+            return (
+                <div>I am Signed in</div>
+            );
+        } 
+        else {
+            return ( <div>I am not signed in</div> );
+        }
+    }
+
+    render() {
+        return (
+        <div>{this.renderAuthButton()}</div>
+        );
+    }
+}
+
+export default GoogleAuth;
