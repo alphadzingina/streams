91a88eeeddefee07817e72cdf3e3d33486a7692a
alphadzingina <alphaadx6@gmail.com>
2020-06-23 23:00:58 +0100

    display sign-in and sign-out buttons

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -19,20 +19,26 @@ class GoogleAuth extends React.Component {
     onAuthChange = () => {
         this.setState({ isSignedIn: this.auth.isSignedIn.get() });
     }
-    
+
     renderAuthButton() {
         if (this.state.isSignedIn === null ) {
-            return (
-                <div>I don't know if we are signed in</div>
-            );
+            return null;
         } 
         else if ( this.state.isSignedIn === true ) {
             return (
-                <div>I am Signed in</div>
+                <button className="ui red google button">
+                    <i className="google icon" />
+                    Sign Out
+                </button>
             );
         } 
         else {
-            return ( <div>I am not signed in</div> );
+            return (
+                <button className="ui red google button">
+                    <i className="google icon" />
+                    Sign In with Google
+                </button>
+            );
         }
     }
 
