763e5cbfba136983475fbbb60debc20d5abcefa0
alphadzingina <alphaadx6@gmail.com>
2020-07-22 10:51:58 +0100

    create modal for delete stream

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import Modal from '../Modal';
+import history from '../../history';
 
 const StreamDelete = () => {
+    const modalActions = (
+        <React.Fragment>
+            <div class="ui button primary">No</div>
+            <div class="ui button negative">Yes</div>
+            </React.Fragment>
+    );
     return(
-        <div>StreamDelete</div>
+        <div>
+            StreamDelete
+            <Modal 
+                title="Delete Stream"
+                content="Are you sure you want to delete this stream?"
+                actions={modalActions}
+                onDismiss={() => history.push('/')}
+            />
+        </div>
     );
 };
 
